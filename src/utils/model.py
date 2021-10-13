60c894e0d91d77e334b906fd5d90d479c452510f
Ketan Gangal <Ketangangal98@gmail.com>
2021-10-13 17:20:33 +0530

    save plot function added

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from logs import logger
+import matplotlib.pyplot as plt
 import os
 import time
 
@@ -29,7 +30,7 @@ def create_model(input_shape, Loss_function, optimizer, metrics, Num_classes, co
 
 
 def get_unique_filename(filename):
-    unique_filename = time.strftime(f"{filename}._%Y_%m_%d_%H_%M_%S.h5")
+    unique_filename = time.strftime(f"{filename}._%Y_%m_%d_%H_%M.h5")
     return unique_filename
 
 
@@ -37,3 +38,24 @@ def save_model(model, name, path):
     unique_name = get_unique_filename(name)
     path_to_model = os.path.join(path, unique_name)
     model.save(path_to_model)
+
+
+def get_unique_plotname(filename):
+    unique_filename = time.strftime(f"{filename}._%Y_%m_%d_%H_%M_%S.png")
+    return unique_filename
+
+
+def save_plot(history, name, path):
+    unique_name1 = get_unique_plotname(name)
+
+    path_to_plot1 = os.path.join(path, unique_name1)
+
+    fig, ax = plt.subplots(nrows=1, ncols=2)  # create figure & 1 axis
+
+    ax[0].plot(history.history['accuracy'], history.history['val_accuracy'])
+    ax[1].plot(history.history['loss'], history.history['val_loss'])
+
+    fig.savefig(path_to_plot1)  # save the figure to file
+
+    plt.close(fig)
+
